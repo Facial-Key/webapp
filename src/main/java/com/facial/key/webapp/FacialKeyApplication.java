fb94e3e70c0191e573d8d0d09fe89173fe3ea260
Hemerson Walviesse <walviesse.dev@gmail.com>
2024-12-02 10:49:50 -0300

    Frist commit

---
+++ src/main/java/com/facial/key/webapp/FacialKeyApplication.java
@@ -0,0 +1,13 @@
+package com.facial.key.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacialKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacialKeyApplication.class, args);
+	}
+
+}
